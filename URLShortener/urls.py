03e2e6aa25f9e6025b772d3016dc859a94372e4b
Mehdi <MehdiMortaz@gmail.com>
2021-04-15 01:23:11 +0430

    added a url to project for route to shortener app

--- URLShortener/urls.py
+++ URLShortener/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url, include
+from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^api/v1/url_shortener/', include('apps.shortener.urls')),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
