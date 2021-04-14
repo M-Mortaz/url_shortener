70c311e0fe6156e6c26577bcc54cbe15b4852b2c
Mehdi <MehdiMortaz@gmail.com>
2021-04-15 01:23:48 +0430

    initialize model related to URL of the shortener app

--- apps/shortener/models.py
+++ apps/shortener/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class URL(models.Model):
+    original_url = models.URLField()
+    short_url = models.CharField(max_length=100, unique=True)
+    visit_count = models.PositiveBigIntegerField(default=0)
+    # creator = models.ForeignKey("User", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_Add=True)
+    update_at = models.DateTimeField(auto_now=True)
