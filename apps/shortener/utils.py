af87daac2c909c2a7a43252941eef5cda6a1a4df
Mehdi <MehdiMortaz@gmail.com>
2021-04-18 22:37:58 +0430

    added docstring to create_random_string

--- apps/shortener/utils.py
+++ apps/shortener/utils.py
@@ -4,8 +4,12 @@ from django.conf import settings
 
 def create_random_string(inc: int = 0) -> str:
     """
-    create random string.
-    inc: Excess amount over the minimum length of the shortener min hash length.
+    create random string. the length depends on the input argument inc. the minimum length will be set to
+     the SHORTENER_MIN_HASH_LENGTH variable in the django settings or environments variable.
+    Args:
+        inc (int): Excess amount over the minimum length of the shortener min hash length.
+    Returns:
+        str: a string with `default_min_length+inc` character sequence
     """
     sequence = string.ascii_letters + string.digits
     return ''.join(random.choices(sequence, k=settings.SHORTENER_MIN_HASH_LENGTH + inc))
