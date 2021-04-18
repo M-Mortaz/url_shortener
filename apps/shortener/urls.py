11f535087aeb21b8b252623121f75c9c046c10a3
Mehdi <MehdiMortaz@gmail.com>
2021-04-18 22:30:56 +0430

    remove slash from url of shortener create route

--- apps/shortener/urls.py
+++ apps/shortener/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from apps.shortener import views
 
 urlpatterns = [
-    url(r'^/$', views.URLShortenerCreateListView.as_view(), name='url_shortener_view'),
+    url(r'^$', views.URLShortenerCreateListView.as_view(), name='url_shortener_view'),
 ]
