a3a744cf77df9304fec49b1930ded7af9bf1ffbf
Mehdi <MehdiMortaz@gmail.com>
2021-04-18 22:33:57 +0430

    implement URLShortenerCreateListView

--- apps/shortener/views.py
+++ apps/shortener/views.py
@@ -1,8 +1,13 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from apps.shortener.serializers import URLSerializer
+from apps.shortener.models import URL
 
 
 class URLShortenerCreateListView(generics.ListCreateAPIView):
-    pass
+    permission_classes = (AllowAny, )
+    serializer_class = URLSerializer
+    queryset = URL
 
 
 class URLShortenerRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
