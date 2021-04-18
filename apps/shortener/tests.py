7418e4f691dc4194906a52588d9d232ab9fa33cd
Mehdi <MehdiMortaz@gmail.com>
2021-04-18 22:16:35 +0430

    added TDD for shortener create url

--- apps/shortener/tests.py
+++ apps/shortener/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.test import Client
+from django.utils import timezone
+from unittest.mock import patch
+from datetime import timedelta
 
-# Create your tests here.
+
+class TestUrlShorter(TestCase):
+
+    # @patch('apps.shortener.something')
+    def setUp(self):
+        self.url = "https://google.com"
+
+    # TDD
+    def test_url_shortener_creation(self):
+        """
+        TDD: try to create a shorter url with successful response.
+        """
+        client = Client()
+        headers = {
+            'content_type': 'application/json'
+        }
+
+        data = {
+            'url': self.url
+        }
+
+        response = client.post(
+            path='/api/v1/url_shortener/',
+            data=data,
+            **headers
+        )
+        self.assertEqual(response.status_code, 201)
