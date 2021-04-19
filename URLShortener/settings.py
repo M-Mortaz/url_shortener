b64f02acec5c367338bda8376167544ee0327aab
Mehdi <MehdiMortaz@gmail.com>
2021-04-19 20:30:50 +0430

    added todo

--- URLShortener/settings.py
+++ URLShortener/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("URL_SHORTENER_SK")
+SECRET_KEY = os.getenv("URL_SHORTENER_SK", "adhg5UYG6FGGJSGEFS%^$SDF")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
-ALLOWED_HOSTS = []  # set on the deployment step.
+# TODO: fill following list in the deployment step
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     # Django built-in apps:
