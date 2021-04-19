efe72114c74862fc0b72e9a16ad20a53ee7a8797
Mehdi <MehdiMortaz@gmail.com>
2021-04-19 23:44:04 +0430

    added test for redirect url

--- apps/redirect/tests.py
+++ apps/redirect/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.test import Client
+from django.utils import timezone
+from unittest.mock import patch
+from datetime import timedelta
+from apps.shortener.models import URL
 
-# Create your tests here.
+
+class TestUrlShorter(TestCase):
+
+    # @patch('apps.shortener.something')
+    def setUp(self):
+        self.url = "https://google.com"
+
+    # TDD
+    def test_redirect(self):
+        """
+        testing if redirect working correctly.
+        """
+        url = URL.objects.create(short_url="test",
+                                 original_url="http://test.com/"
+                                 )
+        client = Client()
+        response = client.get(
+            path=f'/r1/{url.short_url}',
+        )
+        self.assertEqual(response.status_code, 301)
+
