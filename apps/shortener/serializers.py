daae30439b662e5f7554412ae8fc2e0b531578f7
Mehdi <MehdiMortaz@gmail.com>
2021-04-19 20:04:36 +0430

    remove responsiblity of creating short_url from serializer

--- apps/shortener/serializers.py
+++ apps/shortener/serializers.py
@@ -1,6 +1,12 @@
+import logging
+
 from rest_framework import serializers
+
 from apps.shortener.models import URL
-from apps.shortener.utils import get_short_url, url_validator
+from apps.shortener.utils import url_validator
+from utils import set_url_details_redis, delete_url_details_redis
+
+logger = logging.getLogger(__name__)
 
 
 class URLSerializer(serializers.ModelSerializer):
@@ -19,39 +25,24 @@ class URLSerializer(serializers.ModelSerializer):
         Returns:
             str: Validate short_url
         """
-        url_id = self.context["url_id"]  # just in update mode we have id.
-        short_url = get_short_url(value)
+        url_id = self.context.get("url_id")  # just in update mode we have id.
 
-        try:
-            old_short_url = URL.objects.get(id=url_id).short_url
-        except URL.DoesNotExist:
-            raise serializers.ValidationError("url does not exists!")
-
-        if url_id and old_short_url == short_url:  # for update step old and new short_value could be same.
-            return short_url
+        if url_id:  # for update step old and new short_value could be same.
+            try:
+                old_short_url = URL.objects.get(id=url_id).short_url
+            except URL.DoesNotExist:
+                raise serializers.ValidationError("url does not exists!")
+            if old_short_url == value:
+                return value
 
         if value and url_validator(value):
             raise serializers.ValidationError(
                 "custom short_url could not be URL itself.Please try for sequence of string instead of a valid URL!"
             )
 
-        if URL.objects.filter(short_url=short_url).exists():
+        if URL.objects.filter(short_url=value).exists():
             raise serializers.ValidationError("url with this short url already exists.")
-        return short_url
-
-    # def create(self, validated_data):
-    #     """
-    #     Responsible to creating url object.
-    #     Args:
-    #         validated_data: validated clients input data
-    #
-    #     Returns:
-    #         URL: and instance of the URL model.
-    #     """
-    #     url = URL.objects.create(
-    #         **validated_data
-    #     )
-    #     return url
+        return value
 
 
 class UpdateURLSerializer(URLSerializer):
@@ -59,3 +50,4 @@ class UpdateURLSerializer(URLSerializer):
         model = URLSerializer.Meta.model
         fields = URLSerializer.Meta.fields
         read_only_fields = ("created_at", "update_at",)
+
